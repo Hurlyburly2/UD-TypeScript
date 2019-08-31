fafc4d412638c9f94c3c7da7d144d865884fdd81
Hurlyburly2 <dougdougmann@gmail.com>
2019-08-30 20:55:45 -0400

    ch2 complete

---
+++ ch2.ts
@@ -0,0 +1,15 @@
+let nameTwo: string = "Max";
+let ageTwo: number = 27;
+
+let anything;
+anything = 12;
+
+// noUnusedParameters -> in tsconfig.json
+function controlMe(isTrue: boolean, somethingElse: boolean) {
+  let result: number;
+  if (ifTrue) {
+    result = 12;
+  }
+  result = 33;
+  return result;
+}
